84150a2f0a8eed2d56b16597ce2f2494cbf8b643
Kaleem U Siddiqui <kaleem.siddiqui_cs17@gla.ac.in>
2019-02-22 09:09:34 +0530

    add constructor for class data and next

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,7 +5,12 @@ public class SinglyLinkedList {
     private class Node {
 private int data;
 private Node next;
-  }
+
+        private Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 
 }
 
